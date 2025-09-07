6ee36d79ba9000ca104f2eb3a383f33d1ffcc40b
junzhang27 <junzhang27@iflytek.com>
2025-09-07 13:50:57 +0800

    update

--- src/main/java/cn/abel/meta/controller/Demo.java
+++ src/main/java/cn/abel/meta/controller/Demo.java
@@ -1,20 +1,15 @@
 package cn.abel.meta.controller;
 
 import com.unfbx.chatgpt.OpenAiClient;
-import com.unfbx.chatgpt.OpenAiStreamClient;
 import com.unfbx.chatgpt.entity.chat.ChatCompletion;
 import com.unfbx.chatgpt.entity.chat.ChatCompletionResponse;
 import com.unfbx.chatgpt.entity.chat.Message;
-import com.unfbx.chatgpt.entity.completions.Completion;
-import com.unfbx.chatgpt.entity.completions.CompletionResponse;
 import com.unfbx.chatgpt.interceptor.OpenAILogger;
-import com.unfbx.chatgpt.sse.ConsoleEventSourceListener;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.util.Arrays;
-import java.util.concurrent.CountDownLatch;
 
 public class Demo {
 
@@ -35,7 +30,7 @@ public class Demo {
 //                httpLoggingInterceptor);
 
         v2 = OpenAiClient.builder()
-                .apiKey("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")
+                .apiKey("sk-**********************************")
                 .connectTimeout(50)
                 .writeTimeout(50)
                 .readTimeout(50)
@@ -58,35 +53,6 @@ public class Demo {
     public static void main(String[] args) {
         Demo demo = new Demo();
         demo.chat();
-
-
-//        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7890));
-//
-//        //配置api keys
-//        OpenAiClient openAiClient = new OpenAiClient("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",proxy);
-////        OpenAiClient openAiClient = new OpenAiClient("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",60,60,60,null);
-////        OpenAiClient openAiClient = new OpenAiClient("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");
-//        CompletionResponse completions = openAiClient.completions("你好");
-//        Arrays.stream(completions.getChoices()).forEach(System.out::println);
-//
-//        System.out.println(openAiClient.models());
-
-
-
-
-//        OpenAiStreamClient client = new OpenAiStreamClient("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", 60, 60, 60);
-//        ConsoleEventSourceListener eventSourceListener = new ConsoleEventSourceListener();
-//        Completion q = Completion.builder()
-//                .prompt("一句话描述下开心的心情")
-//                .stream(true)
-//                .build();
-//        client.streamCompletions(q, eventSourceListener);
-//        CountDownLatch countDownLatch = new CountDownLatch(1);
-//        try {
-//            countDownLatch.await();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
 
     }
 }
